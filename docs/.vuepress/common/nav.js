49ecee989dc02e40210dedc353cdf62b3c35f5ec
nanayashiki24 <nanayashiki24@outlook.com>
2022-05-14 21:40:13 +0800

    deploy

--- docs/.vuepress/common/nav.js
+++ docs/.vuepress/common/nav.js
@@ -1,12 +1,11 @@
 // nav
-const javaNav = require("../nav/javaNav");
-
+const mrfpNav= require("../nav/mrfpNav");
 module.exports = [
   {
     text: '首页', link: '/'
   },
 
-  javaNav,   //Java导航
+  mrfpNav,  //每日复盘
 
   {
     text: '文件站', link: 'https://pan.241314.xyz/'
---
+++ docs/.vuepress/nav/mrfpNav.js
@@ -0,0 +1,4 @@
+// 每日复盘
+module.exports = {
+    text: '每日复盘', link: '/mrfp/220515',  // 每日复盘
+  }
