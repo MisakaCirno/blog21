0562e960ae924b1f8a0d3ca31a9737b58ec8b74c
nanayashiki24 <nanayashiki24@outlook.com>
2022-05-16 00:04:21 +0800

    deploy

--- docs/.vuepress/nav/tradestudyNav.js
+++ docs/.vuepress/nav/tradestudyNav.js
@@ -3,44 +3,52 @@ module.exports = {
     text: '交易学习', link: '/tradestudy/',  // 交易学习
      items: [
       {
+        text: '从零入门',
+        items: [
+          { text: '从零学炒股', link: '/tradestudy/clxcg/' },   
+          { text: '从零学基本面', link: '/tradestudy/clxjbm/' },  
+          { text: '从零学可转债', link: '/tradestudy/clxkzz/' },      
+        ]
+      },
+      {
         text: '基础学习',
         items: [
           { text: '央行与货币', link: '/tradestudy/yhhb/' },   
-          { text: '股票外汇与期货', link: '/tradestudy/jczs' },  
-          { text: 'A股基础知识', link: '/tradestudy/jczs' },    
-          { text: '股票投资', link: '/tradestudy/jczs' },    
+          { text: '股票外汇与期货', link: '/tradestudy/gpwhqh/' },  
+          { text: 'A股基础知识', link: '/tradestudy/agjc/' },    
+          { text: '股票投资', link: '/tradestudy/gptz/' },    
         ]
       },
       {
         text: '传统技术分析',
         items: [
-          { text: 'K线基础', link: '/tradestudy/jczs' },
-          { text: '波浪理论', link: '/tradestudy/jczs' },
-          { text: '江恩理论', link: '/tradestudy/jczs' },
-          { text: '维科夫量价分析', link: '/tradestudy/jczs' },              
+          { text: 'K线基础', link: '/tradestudy/kx/' },
+          { text: '波浪理论', link: '/tradestudy/bl/' },
+          { text: '江恩理论', link: '/tradestudy/jn/' },
+          { text: '维科夫量价分析', link: '/tradestudy/wkf/' },              
         ]
       },
       {
         text: '策略逻辑与系统',
         items: [
-          { text: '基金策略', link: '/tradestudy/jczs' },
-          { text: '短线日内中高频策略', link: '/tradestudy/jczs' },
-          { text: '经典交易策略逻辑与系统', link: '/tradestudy/jczs' },   
-          { text: '商品期货投资与方法', link: '/tradestudy/jczs' },        
+          { text: '基金策略', link: '/tradestudy/jjcl/' },
+          { text: '短线日内中高频策略', link: '/tradestudy/rn/' },
+          { text: '经典交易策略逻辑与系统', link: '/tradestudy/jdxt/' },   
+          { text: '商品期货投资与方法', link: '/tradestudy/qhtz/' },        
         ]
       },
       {
         text: '交易心理与行为',
         items: [
-          { text: '交易心理学与哲学', link: '/tradestudy/jczs' },
-          { text: '行为金融学', link: '/tradestudy/jczs' },       
+          { text: '交易心理学与哲学', link: '/tradestudy/jcxl/' },
+          { text: '行为金融学', link: '/tradestudy/xwjr/' },       
         ]
       },
       {
         text: '大师系列',
         items: [
-          { text: '日本交易高手系列', link: '/tradestudy/jczs' },
-          { text: '华尔街大师系列', link: '/tradestudy/jczs' },       
+          { text: '日本交易高手系列', link: '/tradestudy/rbgs/' },
+          { text: '华尔街大师系列', link: '/tradestudy/hrjds/' },       
         ]
       },
     ]
