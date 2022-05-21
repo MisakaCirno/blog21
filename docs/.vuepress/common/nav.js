9c28907a7dd6c0cb5e081e7fb3a37a56a2f54b7c
nanayashiki24 <nanayashiki24@outlook.com>
2022-05-21 20:45:47 +0800

    deploy

--- docs/.vuepress/common/nav.js
+++ docs/.vuepress/common/nav.js
@@ -6,6 +6,7 @@ const techNav= require("../nav/techNav");
 const touNav= require("../nav/touNav");
 const zjNav= require("../nav/zjNav");
 const hgNav= require("../nav/hgNav");
+const zlxxNav= require("../nav/zlxxNav");
 module.exports = [
   {
     text: '首页', link: '/'
@@ -15,6 +16,7 @@ module.exports = [
   tradestudyNav,  //交易学习
   tradeupNav,  //交易进阶
   touNav,     //大师偷学
+  zlxxNav,// 策略学习
   zjNav,     //政经学习
   hgNav,    //宏观分析
   techNav,  //技术开发
