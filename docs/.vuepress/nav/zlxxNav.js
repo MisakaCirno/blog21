e110503b561bad8b863a649c6323c6e254349f3f
nanayashiki24 <nanayashiki24@outlook.com>
2022-05-21 19:49:59 +0800

    deploy

---
+++ docs/.vuepress/nav/zlxxNav.js
@@ -0,0 +1,21 @@
+// 策略学习
+module.exports = {
+    text: '策略学习', link: '/clxx/',  // 交易进阶
+     items: [
+      {
+        text: '可转债策略',
+        items: [
+          { text: '打新', link: '/tou/bjcj/'},   
+          { text: '摊大饼', link: '/tou/bjcj/'},   
+        ]
+        
+      },
+      {
+        text: '股票策略',
+        items: [
+          { text: '统计套利', link: '/tou/bj2cj/'},     
+        ]
+      },
+
+    ]
+  }
